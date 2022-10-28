b803f280b00fcc831b6ab4e84d7b629c3d5eacb7
metroz1 <wlstjr132@gmail.com>
2022-10-28 22:33:01 +0900

    feature/signup branch init commit

--- src/main/java/com/example/consolelog/controller/MemberController.java
+++ src/main/java/com/example/consolelog/controller/MemberController.java
@@ -1,10 +1,19 @@
 package com.example.consolelog.controller;
 
 
+import com.example.consolelog.dto.requestDto.MemberReqeustDto;
+import com.example.consolelog.dto.responseDto.ResponseDto;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
 @RequestMapping("/member")
 public class MemberController {
+
+    @PostMapping("/signup")
+    public ResponseDto<?> signup(@RequestBody MemberReqeustDto memberReqeustDto) {
+        return null;
+    }
 }
