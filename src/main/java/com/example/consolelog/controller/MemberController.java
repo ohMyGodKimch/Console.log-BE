ee8f91fdce454ba1588bda45118fae60dccacba5
metroz1 <wlstjr132@gmail.com>
2022-10-28 22:18:47 +0900

    feature/signup branch init commit

--- src/main/java/com/example/consolelog/controller/MemberController.java
+++ src/main/java/com/example/consolelog/controller/MemberController.java
@@ -1,4 +1,10 @@
 package com.example.consolelog.controller;
 
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequestMapping("/member")
 public class MemberController {
 }
